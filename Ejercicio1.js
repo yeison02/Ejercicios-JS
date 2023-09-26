8f1b7474c11afd689a9c0975f54ba350506f1819
yeison02 <yeison.suarezt@gmail.com>
2023-09-26 00:41:43 -0500

    Ejercicio1/Variables

---
+++ Ejercicio1.js
@@ -0,0 +1,10 @@
+
+
+//Confeccionar una programa que defina e inicialice una variable de tipo cadena de caracteres donde almacenemos el nombre de un empleado y otra variable de tipo entera donde almacenar el sueldo. Imprimir cada variable en una línea distinta en pantalla. 
+
+let nombre = "yeison";
+let sueldo = 7000;
+document.write(nombre);
+//Salto de linea en JavasCript
+document.write("<br>")
+document.write(sueldo);
