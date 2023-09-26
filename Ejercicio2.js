0e60e69828cfcfe7aaf1b98d3fd82a1dafc0fa74
yeison02 <yeison.suarezt@gmail.com>
2023-09-26 00:54:24 -0500

    Ejercicio2/entrada de datos por teclado

---
+++ Ejercicio2.js
@@ -0,0 +1,8 @@
+//Confeccionar un programa que permita cargar el nombre de un usuario y su mail por teclado. Mostrar posteriormente los datos en la página HTML. 
+let nombre;
+let email;
+nombre = prompt("Ingrese su nombre: ");
+email = prompt("Ingrese su email: ");
+document.write(nombre);
+document.write("<br>");
+document.write(email);
