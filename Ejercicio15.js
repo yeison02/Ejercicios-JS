801bbbf6df83055f85b4ed346a27da8c5dd8494e
yeison02 <yeison.suarezt@gmail.com>
2023-10-08 23:59:34 -0500

    Ejercicio15/funciones con parametros

--- Ejercicio15.js
+++ Ejercicio15.js
@@ -8,19 +8,54 @@ PROBLEMAS
 
 //SOLUCION 1
 
-function entero(valor1, valor2, valor3) {
-  if (valor1 < valor2 && valor1 < valor3) {
-    document.write("El numero menor es: " + valor1);
-  } else {
-    if (valor2 < valor3) {
-      document.write("El numero menor es: " + valor2);
+/*function mostrarMenor(valor1, valor2, valor3) {
+    if (valor1 < valor2 && valor1 < valor3) {
+      document.write("El numero menor es: " + valor1);
     } else {
-      document.write("El numero menor es: " + valor3);
+      if (valor2 < valor3) {
+        document.write("El numero menor es: " + valor2);
+      } else {
+        document.write("El numero menor es: " + valor3);
+      }
+    }
+  }
+  
+  let valor1 = parseInt(prompt("Ingrese el pimer valor: "));
+  let valor2 = parseInt(prompt("Ingrese el segundo valor: "));
+  let valor3 = parseInt(prompt("Ingrese el tercer valor: "));
+  mostrarMenor(valor1, valor2, valor3);*/
+
+//SOLUCION 2
+
+function numerosOrdenados(num1, num2, num3) {
+  if (num1 < num2 && num1 < num3) {
+    document.write(num1 + " ");
+    if (num2 < num3) {
+      document.write(num2 + " " + num3);
+    } else {
+      document.write(num3 + " " + num2);
+    }
+  } else {
+    if (num2 < num3) {
+      document.write(num2 + " ");
+      if (num1 < num3) {
+        document.write(num1 + " " + num3);
+      } else {
+        document.write(num3 + " " + num1);
+      }
+    } else {
+      document.write(num3 + " ");
+      if (num1 < num2) {
+        document.write(num1 + " " + num2);
+      } else {
+        document.write(num2 + " " + num1);
+      }
     }
   }
 }
 
-let valor1 = parseInt(prompt("Ingrese el pimer valor: "));
-let valor2 = parseInt(prompt("Ingrese el segundo valor: "));
-let valor3 = parseInt(prompt("Ingrese el tercer valor: "));
-entero(valor1, valor2, valor3);
+let val1 = parseInt(prompt("Ingrese el pimer valor: "));
+let val2 = parseInt(prompt("Ingrese el segundo valor: "));
+let val3 = parseInt(prompt("Ingrese el tercer valor: "));
+
+numerosOrdenados(val1, val2, val3);
