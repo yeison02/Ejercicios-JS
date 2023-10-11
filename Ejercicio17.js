3fa18e6617f9ccbcfb9c3ac7a19ee6955e43c501
yeison02 <yeison.suarezt@gmail.com>
2023-10-11 13:21:19 -0500

    Ejercicio17.clase (Date)}

--- Ejercicio17.js
+++ Ejercicio17.js
@@ -16,7 +16,7 @@ PROBLEMAS
 
 //SOLUCION 1
 
-let fecha;
+/*let fecha;
 fecha = new Date();
 let mes = fecha.getMonth();
 if (mes < 4) {
@@ -27,4 +27,69 @@ if (mes < 4) {
   } else {
     document.write("Nos encontramos en el tercer cuatrimestre del año");
   }
+}*/
+
+
+//SOLUCION 2
+
+  function retornarDiaSemana(dia) {
+    switch (dia) {
+        case 0:
+            return 'Domingo';
+        case 1:
+            return 'Lunes';
+        case 2:
+            return 'Martes';
+        case 3:
+            return 'Miércoles';
+        case 4:
+            return 'Jueves';
+        case 5:
+            return 'Viernes';
+        case 6:
+            return 'Sábado';
+        default:
+            return 'día inválido';
+    }
 }
+
+function retornarMes(mes) {
+    switch (mes) {
+        case 0:
+            return 'Enero';
+        case 1:
+            return 'Febrero';
+        case 2:
+            return 'Marzo';
+        case 3:
+            return 'Abril';
+        case 4:
+            return 'Mayo';
+        case 5:
+            return 'Junio';
+        case 6:
+            return 'Julio';
+        case 7:
+            return 'Agosto';
+        case 8:
+            return 'Septiembre';
+        case 9:
+            return 'Octubre';
+        case 10:
+            return 'Noviembre';
+        case 11:
+            return 'Diciembre';
+        default:
+            return 'mes inválido';
+    }
+}
+
+
+function retornarFecha() {
+    let fecha = new Date();
+    let string = 'Hoy es ' + retornarDiaSemana(fecha.getDay()) + ' ' + fecha.getDate() + ' de ' +
+        retornarMes(fecha.getMonth()) + ' de ' + fecha.getFullYear();
+    return string;
+}
+
+document.write(retornarFecha());
